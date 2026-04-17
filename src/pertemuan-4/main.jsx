4f1ff94db945c8d8cb850948688618c6610378b8
robby <robby23si@mahasiswa.pcr.ac.id>
2026-04-17 15:19:30 +0700

    update serach and filter

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -1,12 +1,13 @@
 import { createRoot } from "react-dom/client";
 import './tailwind.css';
-import FrameworkList from "./FrameworkList";
+//import FrameworkList from "./FrameworkList";
+import FrameworkListSearchFilter from "./FrameworkListSearchFilter";
 
 
 
 createRoot(document.getElementById("root"))
     .render(
         <div>
-              <FrameworkList/>
+              <FrameworkListSearchFilter/>
         </div>
     );
